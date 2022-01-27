cd160a5eb8e9fabc719e357855a56cdca8b59a88
ShibanshL <shibansh.w@gmail.com>
2022-01-27 18:51:14 +0530

    check

---
+++ src/Component/Test.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+// import a from './Ani'
+export default class Test extends Component {
+    constructor(){
+        super()
+        this.state={
+            name:'Shibansh Sahoo',
+            data:''
+        }
+        this.a = this.a.bind(this)
+    }
+
+
+
+    a(){
+        document.querySelector('.test').style.background='pink'
+        
+    }
+    // setTimeout(a,2000)
+    // async componentDidMount(e){
+    //     const response= await fetch('https://catfact.ninja/fact')
+    //     const data= await response.json()
+    //     const item=data
+    //     console.log("data is here",item)
+    //     this.setState({
+    //         data:item
+    //     })
+    // }
+
+    render() {
+        return (
+            <div className='test'>
+                this is just a check
+                <br/>{this.state.name}
+                
+                <h1 
+                style={{color:'violet',
+                textDecoration:'underline'}}>
+                    {/* {this.state.data.fact} */}
+                </h1>
+            </div>
+        )
+    }
+}
+
+
+
+
