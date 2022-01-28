06d2ebc804ddf8a1ea034b0a5fc526c08edb0a49
ShibanshL <shibansh.w@gmail.com>
2022-01-28 17:12:37 +0530

    Trying to work with props

---
+++ src/Component#2/New.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function New() {
+  return(<div>
+
+        <div className='H1'>
+            <h1>Shibansh Sahoo</h1>
+        </div>
+
+        <div className='H2'>
+            <h1>Abhishek V Bharadwaj</h1>
+        </div>
+
+        <div className='H3'>
+            <h1>Ritesh Das</h1>
+        </div>
+
+      
+        </div>)
+}
