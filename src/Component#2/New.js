872a8276dff2c388f7ac6572afe44575769e565c
ShibanshL <shibansh.w@gmail.com>
2022-01-31 10:13:01 +0530

    Getting the grasp of usestate and useeffect

--- src/Component#2/New.js
+++ src/Component#2/New.js
@@ -1,14 +1,41 @@
-import React from 'react';
-import Info from './Info';
+import React, { useState, useEffect } from 'react';
+// import Info from './Info';
 export default function New(props) {
     console.log(props)
 
-    function j(params) {
-        return(
-            <h1>Inside the Function J</h1>
-        )
-    }
+    const [data, setData] = useState('')
 
+    // async function j(params) {
+
+    //     const url ='https://catfact.ninja/fact'
+    //     const response = await fetch(url)
+    //     const data1 = await response.json()
+    //     const item = data1
+
+        
+    //     console.log(item)
+
+    //     // setData(dataitem)
+    //     setData(item)
+        
+    // }
+    // j()
+    // setData({
+    //     data=j().item
+    // })
+
+    useEffect(async ()=>{
+        const url ='https://catfact.ninja/fact'
+        const response = await fetch(url)
+        const data1 = await response.json()
+        const item = data1
+
+        
+        console.log(item)
+
+        // setData(dataitem)
+        setData(item)
+    },[])
 
   return(<div>
 
@@ -23,7 +50,7 @@ export default function New(props) {
         <div className='H3'>
             <h1>{props.name}</h1>
         </div> */}
-        {j()}
+        {data.fact}
       
         </div>)
 }
