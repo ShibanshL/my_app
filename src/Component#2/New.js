a3a953862d8ac21c4ce2488171b8dbb3b8d12b54
ShibanshL <shibansh.w@gmail.com>
2022-01-31 09:21:39 +0530

    just checking

--- src/Component#2/New.js
+++ src/Component#2/New.js
@@ -2,6 +2,14 @@ import React from 'react';
 import Info from './Info';
 export default function New(props) {
     console.log(props)
+
+    function j(params) {
+        return(
+            <h1>Inside the Function J</h1>
+        )
+    }
+
+
   return(<div>
 
         {/* <div className='H1'>
@@ -15,7 +23,7 @@ export default function New(props) {
         <div className='H3'>
             <h1>{props.name}</h1>
         </div> */}
-
+        {j}
       
         </div>)
 }
