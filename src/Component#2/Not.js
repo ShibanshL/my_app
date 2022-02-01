980d57efcd47a38941aaca32ca0c862e921d91cf
ShibanshL <shibansh.w@gmail.com>
2022-02-01 13:35:14 +0530

    hey

--- src/Component#2/Not.js
+++ src/Component#2/Not.js
@@ -10,6 +10,22 @@ export default class Not extends Component {
         }
     }
 
+    async componentDidUpdate(){
+     
+
+      const url ='https://www.boredapi.com/api/activity';
+      const repsonse = await fetch(url);
+      const data = await repsonse.json()
+      const item = data
+
+      console.log(item.activity)
+
+      // this.setState({in_data:item})
+    // }
+      document.querySelector('.test').innerHTML=item.activity
+
+    }
+
     // componentDidUpdate(pr,pp,ss){
     //   if(pr<pp)
     //     this.setState({
@@ -19,12 +35,13 @@ export default class Not extends Component {
 
   render() {
     return <div>
-        {this.props.name}
-        {this.state.in_data}
+        {/* {this.props.name} */}
+        {this.state.in_data.activity}
         <div className='ch'></div>
-
+        <div className='test'></div>
         <h1>DATA : {this.props.data1}</h1>
-        <h1>nd : {this.state.nd}</h1>
+        <h1>nd : {this.state.nd}</h1><br></br>
+        <button onClick={()=>this.setState({nd:this.state.nd+1})}>ND Update</button>
 
     </div>;
   }
