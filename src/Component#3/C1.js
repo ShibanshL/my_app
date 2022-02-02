29d8491c27b40a6b7d8319608a1f074308cfc161
ShibanshL <shibansh.w@gmail.com>
2022-02-02 16:10:42 +0530

    hihih

--- src/Component#3/C1.js
+++ src/Component#3/C1.js
@@ -34,7 +34,7 @@ export default class C1 extends Component {
     {this.state.data.map(e=>{
             return(
             <>
-            T&nbsp;=&nbsp;{e.T}&nbsp;&nbsp;=&nbsp;{e.t}&nbsp;c&nbsp;=&nbsp;{e.c}&nbsp;h&nbsp;=&nbsp;{e.h}&nbsp;l&nbsp;=&nbsp;{e.l}&nbsp;n&nbsp;=&nbsp;{e.n}&nbsp;e&nbsp;=&nbsp;{e.e}&nbsp;v&nbsp;=&nbsp;{e.v}<br></br>
+            T&nbsp;=&nbsp;{e.T};&nbsp;&nbsp;=&nbsp;{e.t};&nbsp;c&nbsp;=&nbsp;{e.c};&nbsp;h&nbsp;=&nbsp;{e.h};&nbsp;l&nbsp;=&nbsp;{e.l};&nbsp;n&nbsp;=&nbsp;{e.n};&nbsp;e&nbsp;=&nbsp;{e.e};&nbsp;v&nbsp;=&nbsp;{e.v};<br></br>
             </>)
         })}<br></br>
 
