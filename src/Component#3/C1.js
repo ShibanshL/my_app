616e732e373e7e770196d892d93d55728dc07c96
ShibanshL <shibansh.w@gmail.com>
2022-02-02 13:05:36 +0530

    hhehhe map kinda over but still working

--- src/Component#3/C1.js
+++ src/Component#3/C1.js
@@ -4,21 +4,45 @@ export default class C1 extends Component {
     constructor(){
         super()
         this.state = {
-            data:''
+            data:[]
         }
     }
 
 
-    componentDidMount(){
+   async componentDidMount(){
         const url = 'https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/2020-10-14?adjusted=true&apiKey=O6ZsUfA79Hc84eDrD3lkMLaPbtZDCbVX'
         const response = await fetch(url)
         const data = await response.json()
-        const item = data
+        const item = data.results
 
-        console.log("I = "+item)
+        console.log(item)
+
+        this.setState({
+            data:item
+        })
     }
 
   render() {
-    return <div>hi</div>;
+    return <div>hi
+
+        {this.state.data.queryCount}<br></br>
+
+        {/* {this.state.data.results}<br></br> */}
+
+
+    T = {this.state.data.map(e=>{
+            return(
+            <>
+            {e.T}
+            </>)
+        })}<br></br>
+
+{/* {this.state.data.map(e=>{
+    return<>
+        {e.results.t}
+    </>
+})} */}
+
+    </div>;
   }
 }
