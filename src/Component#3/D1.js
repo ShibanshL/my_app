f97352e23ddf0bc4e03ccbded1f9498e58d7724c
ShibanshL <shibansh.w@gmail.com>
2022-02-07 12:39:47 +0530

    something

--- src/Component#3/D1.js
+++ src/Component#3/D1.js
@@ -1,12 +1,17 @@
 // import React, { Component } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
 import React from 'react';
 
 export default function D1() {
-    const { name } = useParams
+    const { name } = useParams()
+    const nav = useNavigate()
   return <div>
       Just some randome stuff - {name}
+
+      <button onClick={()=>{
+        nav('/C1')
+      }}>CLick to go to C1</button>
   </div>;
 }
 
