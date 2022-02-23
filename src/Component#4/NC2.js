18b45946ba86ac9c6345f62c13a8a8f0d9786277
ShibanshL <shibansh.w@gmail.com>
2022-02-23 11:15:24 +0530

    A bit of clean up job

--- src/Component#4/NC2.js
+++ src/Component#4/NC2.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Link, NavLink } from 'react-router-dom'
-import './NC2.css'
+// import './NC2.css'
 import NC3 from './NC3'
 export default class NC2 extends Component {
    
@@ -10,60 +10,23 @@ export default class NC2 extends Component {
             data:[],
             check:[]
         }
-        this.fetchRequest = this.fetchRequest.bind(this)
     }
 
-    // handleClick(){
-    //  const nav = NavLink()
-    //  nav('/NC3/'+this.state.check+'/')
-    // }
-
-    async fetchRequest(){
-        const url = 'https://random.dog/woof.json'
-        const res = await fetch(url)
-        const data = await res.json()
-
-        // console.log('data '+data)
-        this.setState({check:data})
-
-        console.log(this.state.check)
-
-    }
+  
     async componentDidMount(){
-        // fetch('https://api.publicapis.org/entries')
-        // .this(res => res.json())
-        // .this(data => this.setState({data:data}))
-
         const url = 'https://api.publicapis.org/entries'
         const res = await fetch(url)
         const data = await res.json()
 
-        // console.log(data)
+        console.log(data)
         this.setState({data:data.entries})
 
-       this.fetchRequest()
-        // console.log(this.state.check+'hi')
     }
   render() {
-    //   console.log(this.state.data)
     return (
-      <div>NC2<br/><hr/>
-      <h3>
-          <img src={this.state.check.url} />
-          {/* {this.state.check} */}
-      </h3>
+      <div>
 
-      <h2 className='Try'>
-          {this.state.data.map(e =>{
-              return(
-                  <div className='Try2'>
-                      {e.Link}    
-
-                      <NC3 check={this.state.check}   />           
-                  </div>
-              )
-          })}
-      </h2>
+          
       </div>
     )
   }
---
+++ src/Component#4/NC4_cards.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class NC4_cards extends Component {
+  render() {
+    return (
+      <div className='Main'>
+          <div className='Sub'>
+            <div>Properties</div>
+          </div>
+      </div>
+    )
+  }
+}
